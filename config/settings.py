6e7204dec50c4ff9b29907dccfce2ceffe9b948f
Komp <artemshoshura@mail.ru>
2025-06-23 22:37:51 +0400

    правка

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+
+SECRET_KEY = os.getenv("SECRET_KEY") or "insecure-test-secret-key"
+
 
 DEBUG = os.getenv("DEBUG") == "True"
 
